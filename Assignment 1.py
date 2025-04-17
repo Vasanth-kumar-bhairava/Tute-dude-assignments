9a625904b2f8a281ea089e77b94435e57bc75fb2
Vasanth-kumar-bhairava <vasanth.kumar.b0404@gmail.com>
2025-04-17 19:27:23 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+"""#Task 1
+
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+print("Addition:",x+y ,"\nSubtraction:",x-y ,"\nMultiplication:",x*y,"\nDivision:",x/y)
+"""
+
+
+#Task 2
+
+a=str(input("Enter the first name: "))
+b=str(input("Enter the last name: "))
+print("Hello,",a,b,"!Welcome to the python program")
